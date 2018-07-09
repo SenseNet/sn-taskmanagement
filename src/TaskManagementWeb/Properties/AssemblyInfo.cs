775ae01cd6b23b82c18b97f4c512ee87a7411327
tusmester <tusmester@gmail.com>
2018-07-09 14:04:59 +0200

    Raise version to 1.2.

--- src/TaskManagementWeb/Properties/AssemblyInfo.cs
+++ src/TaskManagementWeb/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 #if DEBUG
 [assembly: AssemblyTitle("SenseNet.TaskManagement.Web (Debug)")]
 #elif DIAGNOSTIC
@@ -11,30 +8,16 @@ using System.Runtime.InteropServices;
 #else
 [assembly: AssemblyTitle("SenseNet.TaskManagement.Web (Release)")]
 #endif
-[assembly: AssemblyDescription("Sense/Net TaskManagement web application")]
+[assembly: AssemblyDescription("sensenet TaskManagement web application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sense/Net Inc.")]
 [assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
-[assembly: AssemblyProduct("Sense/Net TaskManagement")]
+[assembly: AssemblyProduct("sensenet TaskManagement")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2c068f75-2c3c-4024-8f90-59cb46d341bc")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
