a1a499bb4ecc3202186d1952066835295b03c22a
tusmester <tusmester@gmail.com>
2020-07-30 21:39:17 +0200

    Cleanup config class.

--- src/TaskManagementWeb/App_Start/Startup.cs
+++ src/TaskManagementWeb/App_Start/Startup.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-//using System.Net.Http.Formatting;
-using Microsoft.AspNetCore.SignalR;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using System.Threading;
-using SenseNet.TaskManagement.Data;
+﻿using SenseNet.TaskManagement.Data;
 using SenseNet.TaskManagement.Hubs;
-using SenseNet.TaskManagement.Web;
-using SenseNet.TaskManagement.Core;
-//using System.Web.Configuration;
-using System.Configuration;
 using Microsoft.AspNetCore.Builder;
-//using System.Web.Http.ExceptionHandling;
-using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using SenseNet.Diagnostics;
 using SenseNet.Extensions.DependencyInjection;
 using EventId = SenseNet.Diagnostics.EventId;
@@ -107,10 +92,6 @@ namespace SenseNet.TaskManagement.Web
             //UNDONE: add Redis backend if SignalR scale out is enabled
             //if (SenseNet.TaskManagement.Web.Configuration.SignalRSqlEnabled)
             //    GlobalHost.DependencyResolver.UseSqlServer(SenseNet.TaskManagement.Web.Configuration.SignalRDatabaseConnectionString);
-
-            SnLog.WriteInformation(
-                $"SignalR backplane is{(Web.Configuration.SignalRSqlEnabled ? string.Empty : " NOT")} enabled.",
-                EventId.TaskManagement.Lifecycle);
             
             //TODO: configure global error logging
             //httpConfiguration.Services.Add(typeof(IExceptionLogger), new WebExceptionLogger());
--- src/TaskManagementWeb/Configuration.cs
+++ src/TaskManagementWeb/Configuration.cs
@@ -1,40 +1,10 @@
-﻿using System.Configuration;
-
-// ReSharper disable once CheckNamespace
-namespace SenseNet.TaskManagement.Web
+﻿namespace SenseNet.TaskManagement.Web
 {
     public class TaskManagementConfiguration
     {
-        //TODO: put all config values here
-
         /// <summary>
         /// After this timeout the task lock will expire so any agent can claim the task.
         /// </summary>
         public int TaskExecutionTimeoutInSeconds { get; set; }
-    }
-
-    //UNDONE: modernize configuration handling
-    internal static class Configuration
-    {
-        //private static readonly string TaskDatabaseConnectrionStringKey = "TaskDatabase";
-        //private static readonly string SignalRDatabaseConnectrionStringKey = "SignalRDatabase";
-        private static readonly string SignalRSqlEnabledKey = "SignalRSqlEnabled";
-
-        private static bool? _signalRSqlEnabled;
-        internal static bool SignalRSqlEnabled
-        {
-            get
-            {
-                if (!_signalRSqlEnabled.HasValue)
-                {
-                    bool value;
-                    var setting = ConfigurationManager.AppSettings[SignalRSqlEnabledKey];
-                    if (string.IsNullOrEmpty(setting) || !bool.TryParse(setting, out value))
-                        value = false;
-                    _signalRSqlEnabled = value;
-                }
-                return _signalRSqlEnabled.Value;
-            }
-        }
     }
 }
