8170f7be6f37772efb969ddb0a2cdd4e1dd01360
tusmester <tusmester@gmail.com>
2023-05-08 15:03:08 +0200

    Store api keys along other app data. (#46)

---
+++ src/TaskManagement.Core/TaskAuthenticationOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SenseNet.Client.Authentication;
+
+namespace SenseNet.TaskManagement.Core
+{
+    public class TaskAuthenticationOptions : AuthenticationOptions
+    {
+        public const string DefaultTaskType = "Default";
+
+        public string TaskType { get; set; }
+        public DateTime ApiKeyExpiration { get; set; }
+    }
+}
